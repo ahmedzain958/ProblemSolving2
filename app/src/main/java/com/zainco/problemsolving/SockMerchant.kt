7125a4e9a3f3154d9c225d55071970692c3b4b52
Zain <ahmedzain958@gmail.com>
2019-07-28 14:55:11 +0200

    sock merchant

--- app/src/main/java/com/zainco/problemsolving/SockMerchant.kt
+++ app/src/main/java/com/zainco/problemsolving/SockMerchant.kt
@@ -1,8 +1,9 @@
 package com.zainco.problemsolving
 
-import android.os.Build
-import android.support.annotation.RequiresApi
+import java.util.*
+import kotlin.collections.HashMap
 
+/*
 
 fun main() {
     val n = 4
@@ -36,4 +37,40 @@ fun sockMerchant(n: Int, ar: IntArray): Int {
     }
     println(pairs)
     return pairs
+}*/
+
+// Complete the sockMerchant function below.
+fun sockMerchant(n: Int, ar: Array<Int>): Int {
+    val hashSet: HashMap<Int, Int> = HashMap()
+    val iterator = ar.iterator()
+    while (iterator.hasNext()) {
+        val current = iterator.next()
+        if (hashSet.isNullOrEmpty()) {
+            hashSet[current] = 1
+        } else {
+            if (hashSet.contains(current)) {
+                val frequency = hashSet[current]
+                hashSet[current] = frequency!!.plus(1)
+            } else {
+                hashSet[current] = 1
+            }
+        }
+    }
+    var pairs = 0
+    hashSet.forEach { (key, value) ->
+        pairs += value shr 1
+    }
+    return pairs
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+
+    val result = sockMerchant(n, ar)
+
+    println(result)
 }
